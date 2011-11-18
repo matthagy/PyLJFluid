105e162f8f8abcc6a80076e7d42de2cc1938a241
Matt Hagy <hagy@gatech.edu>
2011-11-18 17:07:15 -0500

    Updated demo testing code

--- Demo/test1.py
+++ Demo/test1.py
@@ -6,7 +6,8 @@ from matplotlib import pyplot as plt
 from pyljfluid.components import (Parameters,
                                   NeighborsTable, NeighborsTableTracker,
                                   LJForceFeild, Config,
-                                  System, EnergyMinimzer)
+                                  System, EnergyMinimzer,
+                                  PairCorrelationFunctionCalculator)
 
 
 
@@ -31,61 +32,41 @@ def main():
     config = Config.create(N=N_particles, rho=rho, dt=parameters.delta_t,
                            sigma=forcefield.sigma, T=T, mass=parameters.mass)
 
-    Tc = config.calculate_temperature(mass=parameters.mass)
-
-    neighbors_table.rebuild_neighbors(config.positions, config.box_size)
-    U = forcefield.evaluate_potential(config.positions, config.box_size, neighbors_table)
-
-    print 'init T=%.2f U=%.2e' % (Tc, U / N_particles)
-
-    # neighbors_table.rebuild_neighbors(config.positions, config.box_size)
-    # print 'neighbors', neighbors_table.size
-    # forces = np.zeros_like(config.positions)
-    # forcefield.evaluate_forces(forces, config.positions, config.box_size, neighbors_table)
-    # U = forcefield.evaluate_potential(config.positions, config.box_size, neighbors_table)
-
     minizer = EnergyMinimzer(config, forcefield, maxiter=40, neighbors_table=neighbors_table)
     config = minizer.minimize()
     print 'minimized U=%.2f' % (minizer.U_min / N_particles,)
-
-    neighbors_table.rebuild_neighbors(config.positions, config.box_size)
-    U = forcefield.evaluate_potential(config.positions, config.box_size, neighbors_table)
-    print 'run T=%.2f U=%.2e' % (Tc, U / N_particles)
 
     neighbors_table_tracker = NeighborsTableTracker(neighbors_table, config.box_size)
 
     i_scale = 30
 
-    acc = []
+    pcc = PairCorrelationFunctionCalculator(0.01, config.box_size / 2)
 
     forces = np.empty_like(config.positions)
-    for j in xrange(100):
+    for j in xrange(500):
 
         if j < i_scale:
             config.randomize_velocities(T=T, mass=parameters.mass)
+        else:
+            pcc.accumulate_config(config)
 
-        for i in xrange(50):
+        for i in xrange(25):
             forcefield.evaluate_forces(forces, config.positions % config.box_size,
                                        config.box_size, neighbors_table)
             neighbors_table_tracker.maybe_rebuild_neighbor(config.positions)
-            #forces.fill(0.0)
             config.propagate(forces, parameters.mass)
 
         KE = config.calculate_kinetic_energy()
         Tx = config.calculate_temperature(mass=parameters.mass)
         U = forcefield.evaluate_potential(config.positions % config.box_size,
                                           config.box_size, neighbors_table) / N_particles
-        print '%d T=%.2f K=%.2f U=%.2f H=%.2f' % (
-            j, Tx, KE, U, KE + U)
-        acc.append([Tx, KE, U])
+        print '%d T=%.2f K=%.2f U=%.2f H=%.2f' % ( j, Tx, KE, U, KE + U)
 
-    Ts, Ks, Us = np.array(acc).T
     plt.clf()
-    plt.axvline(i_scale, color='k')
-    plt.plot(Ks, 'b-')
-    plt.plot(Us, 'r-')
-    plt.plot(Us + Ks, 'm-')
+    plt.plot(pcc.calculate_rs(), pcc.calculate_gs())
     plt.show()
+
+
 
 def prof_main():
   import cProfile, pstats, sys
