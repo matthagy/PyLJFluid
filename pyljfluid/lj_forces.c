5308b5fbcb1cdb3b530f2461e608df06b98a59f4
Matt Hagy <hagy@gatech.edu>
2012-04-03 17:11:33 -0400

    Added comments on optimized C-code for LJ force evaluation

--- pyljfluid/lj_forces.c
+++ pyljfluid/lj_forces.c
@@ -1,4 +1,9 @@
-
+/* Optimized code for evaluating Lennard-Jones forces
+ * for a configuration of particles with in a cubic periodic
+ * box.
+ * Currently this code accounts for roughly 70% of simulation
+ * rumtime so optimization is worthwhile.
+ */
 
 #include <stdlib.h>
 #include <stdint.h>
@@ -8,6 +13,12 @@ typedef unsigned int uint;
 
 #define dok3 for (int k=0; k<3; k++)
 
+// Calculates net forces as due to LJ interactions on each particle within the
+// N x 3 force array.
+// Doesn't zero the force array, such that the calculated forces can be
+// accumulated on top of the forces already calculated by another force field.
+// Uses the neighbors table stored in the N_neighbor x 2 array of neighbor
+// indices.
 void
 PyLJFluid_evaluate_LJ_forces(double *OPT_RESTRICT forces,
                              size_t N_neighbors,
@@ -21,10 +32,12 @@ PyLJFluid_evaluate_LJ_forces(double *OPT_RESTRICT forces,
   double sigma2 = sigma * sigma;
   double factor = -4 * 6 * epsilon;
 
-  for (uint i=0; i<N_neighbors; i++) {
-    uint inx_i3 = 3*neighbors[i*2];
-    uint inx_j3 = 3*neighbors[i*2 + 1];
+  // loop over pairs of particles in neighbors tables
+  for (uint neighbor_i=0; neighbor_i<N_neighbors; neighbor_i++) {
+    uint inx_i3 = 3*neighbors[neighbor_i*2];
+    uint inx_j3 = 3*neighbors[neighbor_i*2 + 1];
 
+    // calculate separation vector with periodic boundary condition
     double r_ij[3];
     { double *p_i_base = positions + inx_i3;
       double *p_j_base = positions + inx_j3;
@@ -42,12 +55,19 @@ PyLJFluid_evaluate_LJ_forces(double *OPT_RESTRICT forces,
     dok3 { r_sqr += r_ij[k] * r_ij[k]; }
     if (r_sqr > r_cutoff_sqr) continue;
 
+    // calculate the scalar force
+    // scale it by 1/r such that it includes the normalization term to
+    // make the separation vector a unit vector as needed to calculate
+    // components of the force vector
     double scaled_f; {
       double inv_r_sqr = 1.0 / r_sqr;
       double x2 = sigma2 * inv_r_sqr;
       double x6 = x2*x2*x2;
       scaled_f = factor * inv_r_sqr * (2*x6*x6 - x6);
     }
+
+    // calculate components of the force vector and add to net force
+    // for each particle
     { double *f_i_base = forces + inx_i3;
       dok3 { f_i_base[k] += r_ij[k] * scaled_f; }
     }
